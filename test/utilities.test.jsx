a7d51bdd53299130cc292c2dc60f6362d61983bf
MenSeb <sebastien.menard.github@gmail.com>
2020-11-24 10:30:56 -0500

    update utilities tests

--- test/utilities.test.jsx
+++ test/utilities.test.jsx
@@ -15,6 +15,30 @@ const prefix = 'prefix';
 const separator = '-';
 const suffix = 'suffix';
 const options = { prefix, separator, suffix };
+const variablesColors = {
+    vars: {
+        light: 'FFF',
+        dark: '000',
+    },
+    preVar: 'color',
+    preVal: '#',
+};
+const variablesSize = {
+    vars: {
+        height: 100,
+        width: 50,
+    },
+    preVar: 'size',
+    sufVal: 'px',
+};
+const styleColors = {
+    '--color-light': '#FFF',
+    '--color-dark': '#000',
+};
+const styleSize = {
+    '--size-height': '100px',
+    '--size-width': '50px',
+};
 
 describe('isEmptyString', () => {
     it('returns true only for an empty string', () => {
@@ -24,28 +48,40 @@ describe('isEmptyString', () => {
 });
 
 describe('formatPrefix', () => {
-    it('returns data if prefix is undefined or an empty string', () => {
-        expect(formatPrefix(data, {})).toBe(data);
-        expect(formatPrefix(data, { prefix: '' })).toBe(data);
+    describe('When prefix is undefined or an empty string', () => {
+        it('returns data as a string', () => {
+            expect(formatPrefix(100, {})).toBe('100');
+            expect(formatPrefix(false, {})).toBe('false');
+            expect(formatPrefix(data, {})).toBe(data);
+            expect(formatPrefix(data, { prefix: '' })).toBe(data);
+        });
     });
 
-    it('returns and format data with prefix and separator', () => {
-        expect(formatPrefix(data, options)).toBe(
-            `${prefix}${separator}${data}`,
-        );
+    describe('When prefix is not an empty string', () => {
+        it('returns and format data with prefix and separator', () => {
+            expect(formatPrefix(data, options)).toBe(
+                `${prefix}${separator}${data}`,
+            );
+        });
     });
 });
 
 describe('formatSuffix', () => {
-    it('returns data if suffix is undefined or an empty string', () => {
-        expect(formatSuffix(data, {})).toBe(data);
-        expect(formatSuffix(data, { suffix: '' })).toBe(data);
+    describe('When suffix is undefined or an empty string', () => {
+        it('returns data as a string', () => {
+            expect(formatSuffix(100, {})).toBe('100');
+            expect(formatSuffix(false, {})).toBe('false');
+            expect(formatSuffix(data, {})).toBe(data);
+            expect(formatSuffix(data, { suffix: '' })).toBe(data);
+        });
     });
 
-    it('returns and format data with separator and suffix', () => {
-        expect(formatSuffix(data, options)).toBe(
-            `${data}${separator}${suffix}`,
-        );
+    describe('When suffix is not an empty string', () => {
+        it('returns and format data with separator and suffix', () => {
+            expect(formatSuffix(data, options)).toBe(
+                `${data}${separator}${suffix}`,
+            );
+        });
     });
 });
 
@@ -83,48 +119,14 @@ describe('createVariable', () => {
 
 describe('createVariables', () => {
     it('returns and format an object of variables', () => {
-        expect(
-            createVariables({
-                vars: {
-                    light: 'FFF',
-                    dark: '000',
-                },
-                preVar: 'color',
-                preVal: '#',
-            }),
-        ).toMatchObject({
-            '--color-light': '#FFF',
-            '--color-dark': '#000',
-        });
+        expect(createVariables(variablesColors)).toStrictEqual(styleColors);
     });
 });
 
 describe('createVariablesCSS', () => {
     it('returns and format an array of variables object', () => {
         expect(
-            createVariablesCSS([
-                {
-                    vars: {
-                        light: 'FFF',
-                        dark: '000',
-                    },
-                    preVar: 'color',
-                    preVal: '#',
-                },
-                {
-                    vars: {
-                        height: 100,
-                        width: 50,
-                    },
-                    preVar: 'size',
-                    sufVal: 'px',
-                },
-            ]),
-        ).toStrictEqual({
-            '--color-light': '#FFF',
-            '--color-dark': '#000',
-            '--size-height': '100px',
-            '--size-width': '50px',
-        });
+            createVariablesCSS([variablesColors, variablesSize]),
+        ).toStrictEqual({ ...styleColors, ...styleSize });
     });
 });
