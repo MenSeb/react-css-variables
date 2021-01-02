538d2c4642178934d4e0264a54819f093fced6e7
MenSeb <sebastien.menard.github@gmail.com>
2021-01-02 09:36:22 -0500

    remove coverage folder name

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    coverageDirectory: 'coverage',
     moduleDirectories: ['node_modules', 'src'],
     setupFiles: ['./jest.setup.js'],
     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
