22b1c1ae3063de026da1b8198334a2ca52beef97
MenSeb <sebastien.menard.github@gmail.com>
2021-01-02 09:28:13 -0500

    add externals and update output options

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,11 +3,33 @@ const { merge } = require('webpack-merge');
 const path = require('path');
 
 module.exports = merge(config, {
-    mode: 'production',
     devtool: 'source-map',
     entry: path.resolve(__dirname, 'src/index.js'),
+    externals: {
+        'prop-types': {
+            amd: 'prop-types',
+            commonjs: 'prop-types',
+            commonjs2: 'prop-types',
+            root: 'PropTypes',
+        },
+        react: {
+            amd: 'react',
+            commonjs: 'react',
+            commonjs2: 'react',
+            root: 'React',
+        },
+        'react-dom': {
+            amd: 'react-dom',
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            root: 'ReactDOM',
+        },
+    },
+    mode: 'production',
     output: {
         filename: 'index.js',
+        library: 'ReactCSSVariables',
+        libraryTarget: 'umd',
         path: path.resolve(__dirname, 'dist'),
     },
 });
