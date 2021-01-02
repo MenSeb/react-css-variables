3c91e0d76f321eb83a0e82bfa7192f15b8fc5a72
MenSeb <sebastien.menard.github@gmail.com>
2021-01-02 09:28:49 -0500

    sort dev config options

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,12 +3,12 @@ const { merge } = require('webpack-merge');
 const path = require('path');
 
 module.exports = merge(config, {
-    mode: 'development',
     devServer: {
         contentBase: path.resolve(__dirname, 'demo'),
     },
     devtool: 'inline-source-map',
     entry: path.resolve(__dirname, 'demo/index.js'),
+    mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'demo'),
